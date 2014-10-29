44b3ce180c9854da65b11915c07638c88186bde2
gittinsj <james.gittins@ttsltd.com>
2014-10-29 16:50:17 +0000

    Initial attempt at angular

---
+++ js/angulardope.js
@@ -0,0 +1,14 @@
+﻿angular.module('dopeslingerApp', [])
+    .controller('dopeController', ['scope', function ($scope) {
+        $scope.nsfwVal = false;
+
+        $scope.nsfw = function () {
+            if ($scope.nsfwVal) return "hidden";
+            return "";
+        }
+
+        $scope.toggleNsfw = function () {
+            $scope.nsfwVal = !$scope.nsfwVal;
+        }
+
+    }]);
